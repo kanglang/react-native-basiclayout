3eeed859135b5d944de5fee77614f9e7a2c428c5
langlang.kang <langlang.kang@guojutech.net>
2022-01-20 12:09:11 +0800

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @Author: kanglang
  * @Date: 2020-10-12 14:46:29
  * @LastEditors: kanglang
- * @LastEditTime: 2022-01-19 16:03:19
+ * @LastEditTime: 2022-01-20 12:05:45
  * @Description: 基础容器
  */
 import React from 'react';
@@ -37,7 +37,7 @@ export default Basiclayout;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'F2F3F7',
+    backgroundColor: '#F2F3F7',
   },
 });
 
